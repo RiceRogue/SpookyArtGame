1b804d5c64e34182b219984238097e3e403b446c
May <shanglan.may.li@gmail.com>
2021-11-13 02:48:44 -0500

    majority of the heartbeat stuff done. still needs polishing

--- Assets/Scripts/HeartBeatScript.cs
+++ Assets/Scripts/HeartBeatScript.cs
@@ -7,7 +7,9 @@ public class HeartBeatScript : MonoBehaviour
 {
     public Image heart;
     public float heartSpeed;
+    public float heartThresh;
     public float heartMax;
+    public float heartMeter;
     public int heartState;
     public float heartBeat;
 
@@ -20,20 +22,51 @@ public class HeartBeatScript : MonoBehaviour
     {
         heart = GetComponent<Image>();
         heart.fillAmount = 0;
-        heartState = 1;
-        heartMax = 1;
-        heartBeat = 0;
+        //heartState = 1; //the conditional
+        heartThresh = 1; //threshold for each beat
+        heartMeter = 0;
+        heartMax = 51; //max
+        heartBeat = 0; 
         audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        heartSpeed += Time.deltaTime;
-        heartBeat = heartSpeed / heartMax;
-
         
+        heartSpeed += Time.deltaTime;
+        heartMeter += Time.deltaTime;
+
+        if (heartMeter <= 20) //state 1: normal 
+        {
+            heartThresh = 1;
+            
+        }
+        else if (21 <= heartMeter <= 35) //state 2: slightly fast 
+        {
+            heartThresh = .75;
+
+        }
+        else if (36 <= heartMeter <= 45) //state 3: fast 
+        {
+            heartThresh = .55;
+
+        }
+        else if (46 <= heartMeter <= 50) //state 4: HEY-YOURE-ABOUT-TO-DIE-fast 
+        {
+            heartThresh = .4;
+
+        }
+        else if (heartMeter == 51) //state 5: stroke and death
+        {
+            //game over
+
+        }
+
+        heartBeat = heartSpeed / heartThresh;
+        heart.fillAmount = heartMeter / heartMax;
+
+        heartPlay();
 
 
        
@@ -42,13 +75,10 @@ public class HeartBeatScript : MonoBehaviour
 
     void heartPlay()
     {
-        if (heartBeat == 1)
-        {
-            heartSpeed = heartMax;
+        heartSpeed = heartThresh;
 
-            audio.Play();
-            heartSpeed = 0;
-
-        }
+        audio.Play();
+        heartSpeed = 0;
+ 
     }
 }
